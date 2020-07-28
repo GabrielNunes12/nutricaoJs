05ccc68202601bf5758e8ab9a436d4b4a35b0a8f
Gabriel Nunes <prismaticnetwork12@gmail.com>
2020-07-28 00:25:32 -0300

    fazendo o filtro funcionar

--- introducao-javascript/js/filtrar.js
+++ introducao-javascript/js/filtrar.js
@@ -1,10 +1,27 @@
 let campoFiltro = document.querySelector("#filtrar-paciente");
 
+//criando um listener para o campo de busca
 campoFiltro.addEventListener("input", function () {
   let pacientes = document.querySelectorAll(".paciente");
-  for (let i = 0; i < pacientes.length; i++) {
-    let paciente = pacientes[i];
-    let tableNome = paciente.querySelector("info-nome");
-    let nome = tableNome.textContent;
+  //só fazer a filtragem caso o valor do pai for > 0
+  if (this.value.length > 0) {
+    //fazendo um foor para todos os pacientes da tabela
+    for (var i = 0; i < pacientes.length; i++) {
+      let paciente = pacientes[i];
+      let tableNome = paciente.querySelector(".info-nome");
+      let nome = tableNome.textContent;
+      //fazendo expressão regular
+      let expressao = new RegExp(this.value, "i");
+      if (expressao.test(nome)) {
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  } else {
+    for (let i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      paciente.classList.remove("invisivel");
+    }
   }
 });
